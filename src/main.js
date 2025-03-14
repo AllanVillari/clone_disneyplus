5abffdd7e6847f971858dc1cce5b4df8323e2b18
AllanVillari <allan.villarinho@hotmail.com>
2025-03-14 13:43:10 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,18 +1,42 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
-    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+       const posicaoAtual = window.scrollY;
+       if(posicaoAtual < alturaHero){
+        ocultaElementosDoHeader();
+       } else{
+        exibeElementosDoHeader();
+       }
+    })
+
+    // Seção de atrações, programação das abas
     for(let i = 0; i< buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
             escondeTodasAbas();
             aba.classList.add('shows__list--active');
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--active');
-           
         })
+    }
+
+
+    //Seção FAQ, accordion
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click',abreOuFechaResposta);
+    }
+
+    function abreOuFechaResposta(elemento){
+        const classe = 'faq__questions__item--is-open';
+        const elementoPai = elemento.target.parentNode;
+     elementoPai.classList.toggle(classe);
     }
 
     function removeBotaoAtivo(){
@@ -27,6 +51,14 @@ document.addEventListener('DOMContentLoaded', function(){
         for(let i = 0; i< tabsContainer.length; i++){
             tabsContainer[i].classList.remove('shows__list--active');
         }
+    }
 
+    function ocultaElementosDoHeader(){
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+    function exibeElementosDoHeader(){
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
     }
 })
